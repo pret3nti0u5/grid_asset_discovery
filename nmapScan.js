f270ae455e82acc8d948c7f13d170dd48b8b1529
Nitin Arul <nitinarul68@gmail.com>
2021-07-17 18:06:16 +0000

    few more changes to nmapScan

--- nmapScan.js
+++ nmapScan.js
@@ -5,6 +5,7 @@ const { stdout, stderr } = require('process');
 const { stringify } = require('querystring');
 
 
+
 exec("echo '' ;nmap -sC -sV 192.168.149.174", (error, stdout1, stderr) => {
     if (error) throw error
     if (stderr) throw stderr
@@ -13,5 +14,9 @@ exec("echo '' ;nmap -sC -sV 192.168.149.174", (error, stdout1, stderr) => {
     console.log(`The IPv4 address is: ${ip[0]}`)
     let mac = stdout1.match(/(([\da-fA-F]{2}[-:]){5}[\da-fA-F]{2})/i);
     console.log(`the MAC address is ${mac[0]}`)
+    let domain_adrees = stdout1.match(/Domain: [a-zA-Z]+\.[a-zA-Z0-9]+\./i)
+    console.log(`${domain_adrees}`);
+    let service_info = stdout1.match(/Service Info: [a-zA-Z]+: .+/i)
+    console.log(service_info);
 
 })
