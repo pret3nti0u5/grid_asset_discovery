90fcb1e1d9d9fb2a117a23c88dbca826b5627e34
Nitin Arul <nitinarul68@gmail.com>
2021-07-18 10:20:14 +0000

    added test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+const nmap = require('libnmap');
+const opts = {
+  range: ['192.168.149.186'],
+  json: true,
+  verbose: true,
+  flags: ['-sC', '-sV'],
+};
+
+nmap.scan(opts, function (err, report) {
+  if (err) throw new Error(err);
+  for (let item in report) {
+    console.log(report[item]["host"][0]["hostnames"][0]["hostname"]);
+  }
+//   for (let )
+//  console.log(report["item"]);
+});
