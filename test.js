55e7e8e5dca187272ef38a5ca0dd7234b167990f
pret3nti0u5 <vineetvatsal09@gmail.com>
2021-07-18 16:13:12 +0530

    merged

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 const nmap = require('libnmap');
 const opts = {
-<<<<<<< HEAD
-  range: ['192.168.122.232'],
-=======
   range: ['192.168.149.186'],
->>>>>>> 35af87c5715188ef364ba602321ae1e5975c5f66
   json: true,
   verbose: true,
   flags: ['-sC', '-Pn'],
@@ -13,8 +9,8 @@ const opts = {
 nmap.scan(opts, function (err, report) {
   if (err) throw new Error(err);
   for (let item in report) {
-    console.log(report[item]["host"][0]["hostnames"][0]["hostname"]);
+    console.log(report[item]['host'][0]['hostnames'][0]['hostname']);
   }
-//   for (let )
-//  console.log(report["item"]);
+  //   for (let )
+  //  console.log(report["item"]);
 });
