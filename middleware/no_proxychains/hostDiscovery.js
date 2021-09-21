ea28dd2fe5b5207316e7463424ebe6b8b4cce4af
Nitin Arul <nitinarul68@gmail.com>
2021-09-21 22:08:54 +0400

    final commit final

--- middleware/no_proxychains/hostDiscovery.js
+++ middleware/no_proxychains/hostDiscovery.js
@@ -6,20 +6,20 @@ const nmap_discovery = require('./nmap_discovery');
 //const ip_regex = new RegExp("\(\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b\)", 'g')
 
 const host_discovery = (ip_subnet) => {
-  return new Promise((resolve, reject) => {
-    exec(`echo ''; sudo arp-scan ${ip_subnet}`, (error, stdout1, stderr) => {
-      if (error) throw error;
-      if (stderr) throw stderr;
+	return new Promise((resolve, reject) => {
+		exec(`echo ''; sudo arp-scan ${ip_subnet}`, (error, stdout1, stderr) => {
+			if (error) throw error;
+			if (stderr) throw stderr;
 
-      //  ip_list = stdout1.matchAll(/.+ \(\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b\)/g)
-      ip_list = stdout1.match(
-        /\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/g
-      );
+			//  ip_list = stdout1.matchAll(/.+ \(\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b\)/g)
+			ip_list = stdout1.match(
+				/\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/g
+			);
 
-      ip_list.shift()
-      resolve(ip_list);
-    });
-  });
+			ip_list.shift();
+			resolve(ip_list);
+		});
+	});
 };
 
 // const testFunc = async () => {
@@ -35,6 +35,5 @@ const host_discovery = (ip_subnet) => {
 // };
 
 // testFunc();
-
 
 module.exports = host_discovery;
--- middleware/no_proxychains/nmap_discovery.js
+++ middleware/no_proxychains/nmap_discovery.js
@@ -6,105 +6,105 @@ const { stringify } = require('querystring');
 const port_scanner = require('./port_scanner');
 
 const ldap_fn = async (pc_name) => {
-  const ldapSearch = await import('../AD_search/for_ad.mjs');
-  return ldapSearch['default'](pc_name);
-  //  console.info({ ldapSearch }['default'])
+	const ldapSearch = await import('../AD_search/for_ad.mjs');
+	return ldapSearch['default'](pc_name);
+	//  console.info({ ldapSearch }['default'])
 };
 
 //ldap_fn('HYDRA-DC').then((value) => console.log("The domain value is: " + value));
 // ldap_fn('HYDRA-DC').then((value) => console.log(value));
 
 const net_discovery = async (ip_address, dns_address) => {
-  let ip = ip_address;
-  let mac = '';
-  let domain_address = '';
-  let os = '';
-  let workgroup = '';
-  let hostname = '';
+	let ip = ip_address;
+	let mac = '';
+	let domain_address = '';
+	let os = '';
+	let workgroup = '';
+	let hostname = '';
 
-  const ports = await port_scanner(ip_address, dns_address);
-  // `echo '' ;sudo nmap -sn ${ip} ;nmap -f -T 4 -Pn --script smb-os-discovery.nse -O -sV -p${ports}  ${ip} `,
-  return new Promise((resolve, reject) => {
-    exec(
-      `echo '' ;sudo nmap -sn ${ip} --dns-server ${dns_address};sudo nmap -F -T 4 -Pn --script smb-os-discovery.nse -O -sV --dns-server ${dns_address} ${ip}`,
-      (error, stdout1, stderr) => {
-        // if (error) throw error;
-        console.log(dns_address);
-        if (stderr) console.log(stderr);
-        //console.log(stdout1);
-        // ip = stdout1.match(/\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/i)
-        // ip = ip[0]
-        // console.log(`The IPv4 address is: ${ip[0]}`);
-        mac = stdout1.match(/(([\da-fA-F]{2}[-:]){5}[\da-fA-F]{2})/i);
-        if (mac) {
-          //console.log(`the MAC address is ${mac[0]}`);
-          mac = mac[0];
-        }
+	const ports = await port_scanner(ip_address, dns_address);
+	// `echo '' ;sudo nmap -sn ${ip} ;nmap -f -T 4 -Pn --script smb-os-discovery.nse -O -sV -p${ports}  ${ip} `,
+	return new Promise((resolve, reject) => {
+		exec(
+			`echo '' ;sudo nmap -sn ${ip} --dns-server ${dns_address};sudo nmap -F -T 4 -Pn --script smb-os-discovery.nse -O -sV --dns-server ${dns_address} ${ip}`,
+			(error, stdout1, stderr) => {
+				// if (error) throw error;
+				console.log(dns_address);
+				if (stderr) console.log(stderr);
+				//console.log(stdout1);
+				// ip = stdout1.match(/\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/i)
+				// ip = ip[0]
+				// console.log(`The IPv4 address is: ${ip[0]}`);
+				mac = stdout1.match(/(([\da-fA-F]{2}[-:]){5}[\da-fA-F]{2})/i);
+				if (mac) {
+					//console.log(`the MAC address is ${mac[0]}`);
+					mac = mac[0];
+				}
 
-        // domain_address = stdout1.match(/Domain: [a-zA-Z]+\.[a-zA-Z0-9]+\./i);
-        // if (domain_address) {
-        //     console.log(`${domain_address}`);
-        // }
-        // service_info = stdout1.match(/Service Info: [a-zA-Z]+: .+/i)
-        // console.log(service_info);
-        os = stdout1.match(/OS details: .+/i);
-        if (os) {
-          os = os[0].split(':')[1];
-          console.log(os);
-        }
-        if (os === null) {
-          os = stdout1.match(/OS: [a-zA-Z]+;/i);
-          if (os) {
-            os = os[0].match(/[a-zA-Z]+;$/i)[0].split(';')[0];
-          }
-        }
+				// domain_address = stdout1.match(/Domain: [a-zA-Z]+\.[a-zA-Z0-9]+\./i);
+				// if (domain_address) {
+				//     console.log(`${domain_address}`);
+				// }
+				// service_info = stdout1.match(/Service Info: [a-zA-Z]+: .+/i)
+				// console.log(service_info);
+				os = stdout1.match(/OS details: .+/i);
+				if (os) {
+					os = os[0].split(':')[1];
+					console.log(os);
+				}
+				if (os === null) {
+					os = stdout1.match(/OS: [a-zA-Z]+;/i);
+					if (os) {
+						os = os[0].match(/[a-zA-Z]+;$/i)[0].split(';')[0];
+					}
+				}
 
-        // console.log(os);
-        workgroup = stdout1.match(/Workgroup: [a-zA-Z]+/i);
-        if (workgroup) {
-          workgroup = workgroup[0].split(':')[1];
-          //   console.log(workgroup);
-        }
-        hostname = stdout1.match(/Nmap scan report for .+/i);
-        if (hostname) {
-          hostname = hostname[0].split(' ')[4];
-        }
-        // console.log('lmao')
-        //domain_address = ldap_fn(hostname);
-        // const setDomain = async () => {
+				// console.log(os);
+				workgroup = stdout1.match(/Workgroup: [a-zA-Z]+/i);
+				if (workgroup) {
+					workgroup = workgroup[0].split(':')[1];
+					//   console.log(workgroup);
+				}
+				hostname = stdout1.match(/Nmap scan report for .+/i);
+				if (hostname) {
+					hostname = hostname[0].split(' ')[4];
+				}
+				// console.log('lmao')
+				//domain_address = ldap_fn(hostname);
+				// const setDomain = async () => {
 
-        //     //domain_address = await ldap_fn(hostname);
-        //     console.log(domain_address);
-        // };
-        // setDomain();
+				//     //domain_address = await ldap_fn(hostname);
+				//     console.log(domain_address);
+				// };
+				// setDomain();
 
-        // ldap_fn('HYDRA-DC').then((value) => {
-        //     domain_address = value; console.log(`inside function ${domain_address}`);
-        // });
+				// ldap_fn('HYDRA-DC').then((value) => {
+				//     domain_address = value; console.log(`inside function ${domain_address}`);
+				// });
 
-        // console.log(hostname);
-        const JSON_object = {
-          hostname,
-          ip,
-          mac,
-          domain_address,
-          os,
-          workgroup,
-        };
-        ldap_fn(hostname).then((value) => {
-          domain_address = value;
-          console.log(`inside function ${domain_address}`);
-          JSON_object.domain_address = domain_address;
-          // console.log(hostname);
+				// console.log(hostname);
+				const JSON_object = {
+					hostname,
+					ip,
+					mac,
+					domain_address,
+					os,
+					workgroup,
+				};
+				ldap_fn(hostname).then((value) => {
+					domain_address = value;
+					console.log(`inside function ${domain_address}`);
+					JSON_object.domain_address = domain_address;
+					// console.log(hostname);
 
-          //   console.log(JSON_object);
-          resolve(JSON_object);
-        });
-        // resolve(JSON_object);
-        //   console.log(JSON_object);
-      }
-    );
-  });
+					//   console.log(JSON_object);
+					resolve(JSON_object);
+				});
+				// resolve(JSON_object);
+				//   console.log(JSON_object);
+			}
+		);
+	});
 };
 // const testFunc = async () => {
 //     const nice = await net_discovery('192.168.1.1');
